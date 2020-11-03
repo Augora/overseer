081723cfff1101d15c3b4b075e39d6b2dde6b7fa
kevinbacas <bacas.kevin@hotmail.fr>
2020-11-04 00:26:03 +0100

    Add Augora favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { Button, Flex, Text, Heading, PseudoBox } from '@chakra-ui/core';
 import { signIn, signOut, useSession } from 'next-auth/client';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import { FaUsers } from 'react-icons/fa';
 import { FaHome } from 'react-icons/fa';
@@ -21,6 +22,14 @@ function Header(props) {
       bg={['primary.500', 'primary.500', 'transparent', 'transparent']}
       boxShadow="0 0 10px rgba(0,0,0,.5)"
     >
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Flex alignItems="center" justifyContent="space-between" w="100%">
         <Link href="/">
           <PseudoBox
