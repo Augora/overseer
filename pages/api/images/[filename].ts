4a28c5d68e042ed9124f98d86832fe62e5695d74
kevinbacas <bacas.kevin@hotmail.fr>
2020-11-03 15:07:53 +0100

    Remove console.log + udpate cache to 1hour

--- pages/api/images/[filename].ts
+++ pages/api/images/[filename].ts
@@ -1,8 +1,5 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import {
-  BlobServiceClient,
-  StorageSharedKeyCredential,
-} from "@azure/storage-blob";
+import { NowRequest, NowResponse } from '@vercel/node';
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob';
 
 const account = process.env.AZURE_STORAGE_ACCOUNT;
 const accountKey = process.env.AZURE_STORAGE_ACCESS_KEY;
@@ -14,7 +11,7 @@ const blobServiceClient = new BlobServiceClient(
 );
 
 function getBlobFromAzureStorage(blobName: string) {
-  const containerName = "images";
+  const containerName = 'images';
   const containerClient = blobServiceClient.getContainerClient(containerName);
   const blobClient = containerClient.getBlobClient(blobName);
   return blobClient.download();
@@ -23,13 +20,13 @@ function getBlobFromAzureStorage(blobName: string) {
 function streamToBuffer(readableStream) {
   return new Promise((resolve, reject) => {
     const chunks = [];
-    readableStream.on("data", (data) => {
+    readableStream.on('data', (data) => {
       chunks.push(data instanceof Buffer ? data : Buffer.from(data));
     });
-    readableStream.on("end", () => {
+    readableStream.on('end', () => {
       resolve(Buffer.concat(chunks));
     });
-    readableStream.on("error", reject);
+    readableStream.on('error', reject);
   });
 }
 
@@ -41,21 +38,16 @@ function parseQuery(str: string | string[]) {
 }
 
 export default async (req: NowRequest, res: NowResponse) => {
-  console.log("req.query.filename", parseQuery(req.query.filename));
-  if (req.method === "GET") {
-    const blobContent = await getBlobFromAzureStorage(
-      parseQuery(req.query.filename)
-    );
-    const blobContentAsBuffer = await streamToBuffer(
-      blobContent.readableStreamBody
-    );
+  if (req.method === 'GET') {
+    const blobContent = await getBlobFromAzureStorage(parseQuery(req.query.filename));
+    const blobContentAsBuffer = await streamToBuffer(blobContent.readableStreamBody);
     res.status(200);
-    res.setHeader("Content-Type", blobContent.contentType);
+    res.setHeader('Content-Type', blobContent.contentType);
     res.setHeader(
-      "Cache-Control",
-      blobContent.cacheControl || "s-maxage=1, stale-while-revalidate"
+      'Cache-Control',
+      blobContent.cacheControl || 's-maxage=3600, stale-while-revalidate'
     );
-    res.setHeader("eTag", blobContent.etag);
+    res.setHeader('eTag', blobContent.etag);
     return res.send(blobContentAsBuffer);
   }
   return res.status(404).end();
