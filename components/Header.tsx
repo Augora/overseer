d19a3a57c7049c2e78ce07921ef4f5e40b1bdde2
kevinbacas <bacas.kevin@hotmail.fr>
2020-11-03 11:31:09 +0100

    Fix auth condition on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,20 +21,20 @@ function Header(props) {
       bg={['primary.500', 'primary.500', 'transparent', 'transparent']}
       boxShadow="0 0 10px rgba(0,0,0,.5)"
     >
-      {props.session && (
-        <Flex alignItems="center" justifyContent="space-between" w="100%">
-          <Link href="/">
-            <PseudoBox
-              _hover={{
-                color: 'teal.300',
-                cursor: 'pointer',
-              }}
-            >
-              <Heading as="h1" size="lg">
-                Overseer
-              </Heading>
-            </PseudoBox>
-          </Link>
+      <Flex alignItems="center" justifyContent="space-between" w="100%">
+        <Link href="/">
+          <PseudoBox
+            _hover={{
+              color: 'teal.300',
+              cursor: 'pointer',
+            }}
+          >
+            <Heading as="h1" size="lg">
+              Overseer
+            </Heading>
+          </PseudoBox>
+        </Link>
+        {props.session && (
           <Flex justifyContent="space-around">
             <PseudoBox
               mx="2"
@@ -66,11 +66,11 @@ function Header(props) {
               </Link>
             </PseudoBox>
           </Flex>
-          <Button onClick={props.session ? signOut : signIn} variant="solid" size="lg">
-            {props.session ? 'Sign out' : 'Sign in'}
-          </Button>
-        </Flex>
-      )}
+        )}
+        <Button onClick={props.session ? signOut : signIn} variant="solid" size="lg">
+          {props.session ? 'Sign out' : 'Sign in'}
+        </Button>
+      </Flex>
     </Flex>
   );
 }
