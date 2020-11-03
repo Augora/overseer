2326d03335c5135a1a62837e9793de6ddff4aa08
kevinbacas <bacas.kevin@hotmail.fr>
2020-11-03 11:20:14 +0100

    Change top navbar with icons (temporary fix for mobile)

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@ import { signIn, signOut, useSession } from 'next-auth/client';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { FaUsers } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
+
 function Header(props) {
   const router = useRouter();
 
@@ -18,67 +21,55 @@ function Header(props) {
       bg={['primary.500', 'primary.500', 'transparent', 'transparent']}
       boxShadow="0 0 10px rgba(0,0,0,.5)"
     >
-      {!props.session && (
-        <>
-          <Heading as="h1" size="lg">
-            Overseer
-          </Heading>
-          <Button onClick={signIn} variant="solid" size="lg">
-            Sign in
-          </Button>
-        </>
-      )}
       {props.session && (
-        <>
-          <Flex alignItems="center" justifyContent="space-between" w="100%">
-            <Link href="/">
-              <PseudoBox
-                _hover={{
-                  color: 'teal.300',
-                  cursor: 'pointer',
-                }}
-              >
-                <Heading as="h1" size="lg">
-                  Overseer
-                </Heading>
-              </PseudoBox>
-            </Link>
-            <Flex justifyContent="space-around">
-              <PseudoBox
-                mx="2"
-                _hover={{
-                  color: 'teal.300',
-                  cursor: 'pointer',
-                }}
-              >
-                <Link href="/">
-                  <Text fontSize="2xl" color={router.pathname === '/' ? 'teal.300' : 'white.300'}>
-                    Home
-                  </Text>
-                </Link>
-              </PseudoBox>
-              <PseudoBox
-                mx="2"
-                _hover={{
-                  color: 'teal.300',
-                  cursor: 'pointer',
-                }}
-              >
-                <Link href="/groupes-parlementaires">
-                  <Text
-                    fontSize="2xl"
-                    color={router.pathname === '/groupes-parlementaires' ? 'teal.300' : 'white.300'}
-                  >
-                    Groupes Parlementaires
-                  </Text>
-                </Link>
-              </PseudoBox>
-            </Flex>
-            <Button onClick={signOut} variant="solid" size="lg">
-              Sign out
-            </Button>
+        <Flex alignItems="center" justifyContent="space-between" w="100%">
+          <Link href="/">
+            <PseudoBox
+              _hover={{
+                color: 'teal.300',
+                cursor: 'pointer',
+              }}
+            >
+              <Heading as="h1" size="lg">
+                Overseer
+              </Heading>
+            </PseudoBox>
+          </Link>
+          <Flex justifyContent="space-around">
+            <PseudoBox
+              mx="2"
+              _hover={{
+                color: 'teal.300',
+                cursor: 'pointer',
+              }}
+            >
+              <Link href="/">
+                <Text fontSize="2xl" color={router.pathname === '/' ? 'teal.300' : 'white.300'}>
+                  <FaHome />
+                </Text>
+              </Link>
+            </PseudoBox>
+            <PseudoBox
+              mx="2"
+              _hover={{
+                color: 'teal.300',
+                cursor: 'pointer',
+              }}
+            >
+              <Link href="/groupes-parlementaires">
+                <Text
+                  fontSize="2xl"
+                  color={router.pathname === '/groupes-parlementaires' ? 'teal.300' : 'white.300'}
+                >
+                  <FaUsers />
+                </Text>
+              </Link>
+            </PseudoBox>
           </Flex>
-        </>
+          <Button onClick={props.session ? signOut : signIn} variant="solid" size="lg">
+            {props.session ? 'Sign out' : 'Sign in'}
+          </Button>
+        </Flex>
       )}
     </Flex>
   );
