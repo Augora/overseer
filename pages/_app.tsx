7810845880f17b3528187cf53b7a35431d052573
KevinBacas <bacas.kevin@hotmail.fr>
2022-02-02 23:41:09 +0100

    Remove color mode manager from chaka ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, cookieStorageManager, localStorageManager } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Auth } from '@supabase/ui';
@@ -13,15 +13,10 @@ import '@fontsource/roboto-slab/400.css';
 
 const queryClient = new QueryClient();
 
-function getColorModeManager(cookies) {
-  return typeof cookies === 'string' ? cookieStorageManager(cookies) : localStorageManager;
-}
-
 export default function MyApp({ Component, pageProps }) {
-  const colorModeManager = getColorModeManager(pageProps.cookies);
   return (
     <Auth.UserContextProvider supabaseClient={supabase}>
-      <ChakraProvider theme={customTheme} colorModeManager={colorModeManager}>
+      <ChakraProvider theme={customTheme}>
         <QueryClientProvider client={queryClient}>
           <Header {...pageProps} />
           <Component {...pageProps} />
