b774f6675527dacead1ddbc99caed2d0081011f7
KevinBacas <bacas.kevin@hotmail.fr>
2022-02-02 14:59:02 +0100

    Add tailwind forms plugin and redo checkbox for users admin role

--- components/users/UserBox.tsx
+++ components/users/UserBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Checkbox } from '@chakra-ui/react';
 import { ScaleFade } from '@chakra-ui/transition';
@@ -12,6 +12,8 @@ interface IUserBoxProps {
 }
 
 export default function UserBox(props: IUserBoxProps) {
+  const [isAdmin, setIsAdmin] = useState(props.userRole?.Role === 'Admin');
+
   return (
     <ScaleFade initialScale={0.9} in={true}>
       <div className="rounded-md p-5 bg-gray-900 hover:bg-gray-700 transition-colors ease-out delay-100">
@@ -27,20 +29,27 @@ export default function UserBox(props: IUserBoxProps) {
             alt="Profile image"
           />
         </div>
-        <Checkbox
-          defaultIsChecked={props.userRole?.Role === 'Admin'}
-          colorScheme="teal"
-          onChange={(e) => {
-            UpsertUserRoleToSupabase(
-              Object.assign({}, props.userRole, {
-                Role: e.target.checked ? 'Admin' : 'Member',
-                UserId: props.user.id,
-              })
-            );
-          }}
-        >
-          Is admin?
-        </Checkbox>
+        <div className="block">
+          <div className="mt-2">
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                className="w-6 h-6 text-teal-500 border-0 rounded-md focus:ring-0"
+                checked={isAdmin}
+                onChange={() => {
+                  setIsAdmin(!isAdmin);
+                  UpsertUserRoleToSupabase(
+                    Object.assign({}, props.userRole, {
+                      Role: !isAdmin ? 'Admin' : 'Member',
+                      UserId: props.user.id,
+                    })
+                  );
+                }}
+              />
+              <span className="ml-2">Is admin?</span>
+            </label>
+          </div>
+        </div>
       </div>
     </ScaleFade>
   );
