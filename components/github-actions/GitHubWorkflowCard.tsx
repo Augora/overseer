2a2fbbccc86e40f606aee34716c43b577cc6a349
KevinBacas <bacas.kevin@hotmail.fr>
2022-02-17 00:08:08 +0100

    Better icons + tooltips

--- components/github-actions/GitHubWorkflowCard.tsx
+++ components/github-actions/GitHubWorkflowCard.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Box, Button, Heading, IconButton, Text } from '@chakra-ui/react';
 import Link from 'next/link';
 import { formatDistanceToNow, parseJSON } from 'date-fns';
-import { FaSync, FaCheck, FaCross, FaSlash, FaXing, FaCog } from 'react-icons/fa';
+import { ImCross } from 'react-icons/im';
+import { FaSync, FaCheck, FaSlash, FaCog } from 'react-icons/fa';
 import { useQuery, UseQueryResult } from 'react-query';
 import { GetJobs } from '../../lib/github/Workflows';
 import Spinner from '../common/Spinner';
@@ -26,6 +27,7 @@ const jobStatusToBg = {
   success: 'bg-green-500',
   failure: 'bg-red-500',
   in_progress: 'bg-yellow-500',
+  skipped: 'bg-gray-500',
 };
 
 export default function GitHubWorkflowCard(props: IGitHubWorkflowCardProps) {
@@ -73,11 +75,17 @@ export default function GitHubWorkflowCard(props: IGitHubWorkflowCardProps) {
           {data.jobs.map((j) => {
             const currentStatus = j.status === 'completed' ? j.conclusion : j.status;
             return (
-              <div className={`rounded-full w-6 h-6 ${jobStatusToBg[currentStatus]} p-1.5`}>
+              <div
+                className={`relative has-tooltip rounded-full w-6 h-6 ${jobStatusToBg[currentStatus]} p-1.5`}
+              >
                 {currentStatus === 'cancelled' && <FaSlash fontSize={12} />}
                 {currentStatus === 'in_progress' && <FaCog fontSize={12} />}
-                {currentStatus === 'failure' && <FaCross fontSize={12} />}
+                {currentStatus === 'failure' && <ImCross fontSize={12} />}
                 {currentStatus === 'success' && <FaCheck fontSize={12} />}
+                {currentStatus === 'skipped' && <FaSlash fontSize={12} />}
+                <span className="tooltip absolute top-3/4 left-1/2 opacity-0 bg-gray-500 rounded px-4 py-2 min-w-max transition-all">
+                  {j.name}
+                </span>
               </div>
             );
           })}
