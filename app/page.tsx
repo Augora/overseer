e3570544a3b90c164ba7abb9d45ba365a1a8da69
KevinBacas <bacas.kevin@hotmail.fr>
2024-07-19 18:39:34 +0200

    Add error when not logged in on index

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Session } from '@supabase/supabase-js';
+
+import supabase from '../lib/supabase/Client';
 import GroupesHandler from '../components/groupes-parlementaires/GroupesHandler';
 
 export default function Page() {
-  return (
-    <div>
-      <GroupesHandler />
-    </div>
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return session === null ? (
+    <span className="flex justify-center items-center h-screen text-red-500 text-4xl">
+      {'You must log in first.'}
+    </span>
+  ) : (
+    <GroupesHandler />
   );
 }
